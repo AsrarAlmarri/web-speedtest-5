b421077b0bc5769f5439632621277f481b4449d2
AsrarAlmarri <76258365+AsrarAlmarri@users.noreply.github.com>
2021-01-05 16:51:46 +0300

    done

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,23 @@
 // قم بإنشاء الدالة التالية
-
 /*
  * greet(name) اسم الدالة
  * - name قد تستقبل وقد لا تستقبل
  * - name إن لم تستقبل اسم أو "Hello" تطبع
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
+let x = `Someone`
 
-function greet(name) {}
+function greet(name) {
+    
+    if(name != null)
+    {
+    console.log(`Hello ${name}`);
+    }
+
+    else 
+    console.log(`Hello`);
+}
+greet();
+greet(x);
+
+
